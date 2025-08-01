07f3793178b2c26ad94d1d84ea3c056740e3c014
Priyanka <prirath20@gmail.com>
2025-08-01 15:51:11 -0700

    to do list project

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,17 +6,41 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct ContentView: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                Button{
+                    withAnimation{
+                        showNewTask = true
+                    }
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }
         }
         .padding()
+        Spacer()
+        List{
+            ForEach(toDos){toDoItem in
+                Text(toDoItem.title)
+            }
+        }
+        if showNewTask{
+            NewToDo()
+        }
     }
+
 }
 
 #Preview {
